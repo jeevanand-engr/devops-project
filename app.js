b00dba4c0f73c359933e1de31a85fa6d15dfd69b
Periasamy <DWO04VP@vw.vwgroup.com>
2026-05-06 11:57:26 +0530

    updated port for render

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get("/hello", (req, res) => {
   res.json({ message: "Hello DevOps" });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
